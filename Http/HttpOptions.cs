ceb6018b4acf700066746d8ee66dbd7e5e1c54d9
devcoldnoodles <devcoldnoodles@gmail.com>
2020-03-23 23:25:47 +0900

    clear cache

--- Http/HttpOptions.cs
+++ Http/HttpOptions.cs
@@ -7,8 +7,6 @@ namespace HyperSocket.Http
         /* Binding Option */
         public Version Version = new Version(1,1);
         public int Port = 80; // Server Address
-        public int ThreadCount = Math.Max(Environment.ProcessorCount - 1, 1); // Service thread count, recommand count is processer count - 1
-        public int ThreadTick = 10;
         public int BufferSize = 1048576;
         public int BacklogSize = 256;
         public int MaxConnections = 256;
